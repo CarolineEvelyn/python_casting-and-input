9d303b237813c595d87060f88de394ae833f03eb
CarolineEvelyn <carolineevelyn08aguiar@gmail.com>
2024-08-15 21:00:46 -0300

    Add files via upload

---
+++ terceiro.dia.py
@@ -0,0 +1,37 @@
+
+# INTEIRO: ... -1 -2 -3 0 1 2 3 4 ...
+# FLOAT: 5.99 1.52 3.14 
+# STRING: 'Davi', 'ABC', 'Caroline'
+#           'O rato roeu a roupa do rei de roma!'
+#           'O preço do feijão é: R$ 5.99'
+ 
+#           '6.85' -> 6.85
+#           '3' -> 3
+#           9.8 -> 9
+#           16 -> 16.0
+
+texto = '5.99'
+valor = '8.3'
+
+numero = float(texto)
+seg_numero = float(valor)
+
+somar = numero + seg_numero
+print(somar)
+
+x = '5'
+y = '8'
+
+x = int(x) 
+y = int(y)
+
+print(x + y)
+
+valor = int(8.9)
+print(valor)
+
+valor = float(12)
+print(valor)
+
+texto = str(9.7)
+print(texto)
---
+++ terceiro.dia.py_input.py
@@ -0,0 +1,22 @@
+
+# 1a entrada dos dados
+# 2a processamento dos dados
+# 3a saída dos dados
+
+entrada = input('digite qualquer coisa: ')
+
+print(entrada)
+
+nome = input('digite seu nome: ')
+
+print('seu nome é: ', nome)
+
+numero = input('digite seu numero: ')
+numero = int(numero)
+
+print(numero)
+
+idade = input('digite sua idade: ')
+idade = int(idade)
+
+print('sua idade é: ', idade)
